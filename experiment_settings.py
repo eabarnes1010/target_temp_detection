2b7f702728e3f6122b6c36ebdddc4f90323b692c
Elizabeth Barnes <eabarnes@colostate.edu>
2022-03-24 06:53:09 -0600

    added figures for hyperparameter choices

--- experiment_settings.py
+++ experiment_settings.py
@@ -12,7 +12,7 @@ __date__   = "20 March 2022"
 def get_settings(experiment_name):
     experiments = {  
         
-        "exp0": {
+        "exp15C_370": { # formerly exp0
             "save_model": True,
             "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
@@ -37,11 +37,144 @@ def get_settings(experiment_name):
             "act_fun": ["relu","relu"],
             "n_epochs": 25_000,
             "patience": 50,
+        },    
+        
+        "exp11C_370": { #formerly exp10
+            "save_model": True,
+            "n_models": 20,            # the number of networks to train
+            "ssp" : "370",             #[options: '126' or '370']
+            "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
+            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
+            "target_temp": 1.1,
+            "n_train_val_test" : (7,2,1),
+            "baseline_yr_bounds": (1850,1899),
+            "training_yr_bounds": (1970,2100),
+            "anomaly_yr_bounds": (1951,1980),
+            "anomalies": True,         #[options: True or False]
+            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
+
+            "network_type": 'shash2',  #[options: "reg" or "shash2"]
+            "hiddens": [10,10],
+            "dropout_rate": 0.,
+            "ridge_param": [1.0,0.0], 
+            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
+            "batch_size": 64,
+            "rng_seed": 8889,
+            "seed": None,
+            "act_fun": ["relu","relu"],
+            "n_epochs": 25_000,
+            "patience": 50,
+        },  
+        "exp20C_370": { #formerly exp11
+            "save_model": True,
+            "n_models": 20,            # the number of networks to train
+            "ssp" : "370",             #[options: '126' or '370']
+            "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
+            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
+            "target_temp": 2.0,
+            "n_train_val_test" : (7,2,1),
+            "baseline_yr_bounds": (1850,1899),
+            "training_yr_bounds": (1970,2100),
+            "anomaly_yr_bounds": (1951,1980),
+            "anomalies": True,         #[options: True or False]
+            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
+
+            "network_type": 'shash2',  #[options: "reg" or "shash2"]
+            "hiddens": [10,10],
+            "dropout_rate": 0.,
+            "ridge_param": [1.0,0.0], 
+            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
+            "batch_size": 64,
+            "rng_seed": 8889,
+            "seed": None,
+            "act_fun": ["relu","relu"],
+            "n_epochs": 25_000,
+            "patience": 50,
         },        
+        "exp15C_126": { #formerly exp12
+            "save_model": True,
+            "n_models": 20,            # the number of networks to train
+            "ssp" : "126",             #[options: '126' or '370']
+            "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
+            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
+            "target_temp": 1.5,
+            "n_train_val_test" : (7,2,1),
+            "baseline_yr_bounds": (1850,1899),
+            "training_yr_bounds": (1970,2100),
+            "anomaly_yr_bounds": (1951,1980),
+            "anomalies": True,         #[options: True or False]
+            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
+
+            "network_type": 'shash2',  #[options: "reg" or "shash2"]
+            "hiddens": [10,10],
+            "dropout_rate": 0.,
+            "ridge_param": [1.0,0.0], 
+            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
+            "batch_size": 64,
+            "rng_seed": 8889,
+            "seed": None,
+            "act_fun": ["relu","relu"],
+            "n_epochs": 25_000,
+            "patience": 50,
+        },        
+        "exp20C_126": { #formerly exp13
+            "save_model": True,
+            "n_models": 20,            # the number of networks to train
+            "ssp" : "126",             #[options: '126' or '370']
+            "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
+            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
+            "target_temp": 2.0,
+            "n_train_val_test" : (7,2,1),
+            "baseline_yr_bounds": (1850,1899),
+            "training_yr_bounds": (1970,2100),
+            "anomaly_yr_bounds": (1951,1980),
+            "anomalies": True,         #[options: True or False]
+            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
+
+            "network_type": 'shash2',  #[options: "reg" or "shash2"]
+            "hiddens": [10,10],
+            "dropout_rate": 0.,
+            "ridge_param": [1.0,0.0], 
+            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
+            "batch_size": 64,
+            "rng_seed": 8889,
+            "seed": None,
+            "act_fun": ["relu","relu"],
+            "n_epochs": 25_000,
+            "patience": 50,
+        },        
+        "exp20C_126_uniform": { #formerly exp14
+            "save_model": True,
+            "n_models": 1,            # the number of networks to train
+            "ssp" : "126",             #[options: '126' or '370']
+            "gcmsub" : "UNIFORM",          #[options: 'ALL' or 'UNIFORM'
+            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
+            "target_temp": 1.5,
+            "n_train_val_test" : (7,2,1),
+            "baseline_yr_bounds": (1850,1899),
+            "training_yr_bounds": (1970,2100),
+            "anomaly_yr_bounds": (1951,1980),
+            "anomalies": True,         #[options: True or False]
+            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
+
+            "network_type": 'shash2',  #[options: "reg" or "shash2"]
+            "hiddens": [10,10],
+            "dropout_rate": 0.,
+            "ridge_param": [1.0,0.0], 
+            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
+            "batch_size": 64,
+            "rng_seed": 8889,
+            "seed": None,
+            "act_fun": ["relu","relu"],
+            "n_epochs": 25_000,
+            "patience": 50,
+        },          
+        
+        #---------------------- HYPERPARAMETER TUNING ---------------------------
 
         "exp1": {
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
@@ -67,7 +200,7 @@ def get_settings(experiment_name):
         },        
         "exp2": {
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
@@ -119,7 +252,7 @@ def get_settings(experiment_name):
         },   
         "exp4": {
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
@@ -145,7 +278,7 @@ def get_settings(experiment_name):
         }, 
         "exp5": {
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
@@ -171,7 +304,7 @@ def get_settings(experiment_name):
         },      
         "exp6": {
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
@@ -197,7 +330,7 @@ def get_settings(experiment_name):
         },         
         "exp7": {
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
@@ -220,15 +353,15 @@ def get_settings(experiment_name):
             "act_fun": ["relu","relu"],
             "n_epochs": 25_000,
             "patience": 50,
-        },         
+        },      
         
-        "exp10": {
+        "exp10": { 
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
-            "target_temp": 1.1,
+            "target_temp": 1.5,
             "n_train_val_test" : (7,2,1),
             "baseline_yr_bounds": (1850,1899),
             "training_yr_bounds": (1970,2100),
@@ -237,7 +370,7 @@ def get_settings(experiment_name):
             "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
 
             "network_type": 'shash2',  #[options: "reg" or "shash2"]
-            "hiddens": [10,10],
+            "hiddens": [5,5],
             "dropout_rate": 0.,
             "ridge_param": [1.0,0.0], 
             "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
@@ -248,13 +381,13 @@ def get_settings(experiment_name):
             "n_epochs": 25_000,
             "patience": 50,
         },  
-        "exp11": {
+        "exp11": { 
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
+            "n_models": 20,            # the number of networks to train
             "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
-            "target_temp": 2.0,
+            "target_temp": 1.5,
             "n_train_val_test" : (7,2,1),
             "baseline_yr_bounds": (1850,1899),
             "training_yr_bounds": (1970,2100),
@@ -263,7 +396,7 @@ def get_settings(experiment_name):
             "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
 
             "network_type": 'shash2',  #[options: "reg" or "shash2"]
-            "hiddens": [10,10],
+            "hiddens": [25,25],
             "dropout_rate": 0.,
             "ridge_param": [1.0,0.0], 
             "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
@@ -273,11 +406,11 @@ def get_settings(experiment_name):
             "act_fun": ["relu","relu"],
             "n_epochs": 25_000,
             "patience": 50,
-        },        
-        "exp12": {
+        },   
+        "exp12": { 
             "save_model": True,
-            "n_models": 10,            # the number of networks to train
-            "ssp" : "126",             #[options: '126' or '370']
+            "n_models": 20,            # the number of networks to train
+            "ssp" : "370",             #[options: '126' or '370']
             "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
             "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
             "target_temp": 1.5,
@@ -289,7 +422,7 @@ def get_settings(experiment_name):
             "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
 
             "network_type": 'shash2',  #[options: "reg" or "shash2"]
-            "hiddens": [10,10],
+            "hiddens": [10,],
             "dropout_rate": 0.,
             "ridge_param": [1.0,0.0], 
             "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
@@ -299,62 +432,8 @@ def get_settings(experiment_name):
             "act_fun": ["relu","relu"],
             "n_epochs": 25_000,
             "patience": 50,
-        },        
-        "exp13": {
-            "save_model": True,
-            "n_models": 10,            # the number of networks to train
-            "ssp" : "126",             #[options: '126' or '370']
-            "gcmsub" : "ALL",          #[options: 'ALL' or 'UNIFORM'
-            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
-            "target_temp": 2.0,
-            "n_train_val_test" : (7,2,1),
-            "baseline_yr_bounds": (1850,1899),
-            "training_yr_bounds": (1970,2100),
-            "anomaly_yr_bounds": (1951,1980),
-            "anomalies": True,         #[options: True or False]
-            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
-
-            "network_type": 'shash2',  #[options: "reg" or "shash2"]
-            "hiddens": [10,10],
-            "dropout_rate": 0.,
-            "ridge_param": [1.0,0.0], 
-            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
-            "batch_size": 64,
-            "rng_seed": 8889,
-            "seed": None,
-            "act_fun": ["relu","relu"],
-            "n_epochs": 25_000,
-            "patience": 50,
-        },        
-        "exp14": {
-            "save_model": True,
-            "n_models": 10,            # the number of networks to train
-            "ssp" : "126",             #[options: '126' or '370']
-            "gcmsub" : "UNIFORM",          #[options: 'ALL' or 'UNIFORM'
-            "obsdata" : "BEST",        #[options: 'BEST' or 'GISTEMP'
-            "target_temp": 1.5,
-            "n_train_val_test" : (7,2,1),
-            "baseline_yr_bounds": (1850,1899),
-            "training_yr_bounds": (1970,2100),
-            "anomaly_yr_bounds": (1951,1980),
-            "anomalies": True,         #[options: True or False]
-            "remove_map_mean": False,  #[options: False or "weighted" or "raw"]
-
-            "network_type": 'shash2',  #[options: "reg" or "shash2"]
-            "hiddens": [10,10],
-            "dropout_rate": 0.,
-            "ridge_param": [1.0,0.0], 
-            "learning_rate": 0.00001,  # reg->0.0001, shash2->.00005 or .00001
-            "batch_size": 64,
-            "rng_seed": 8889,
-            "seed": None,
-            "act_fun": ["relu","relu"],
-            "n_epochs": 25_000,
-            "patience": 50,
-        },          
-        
-
-        
+        },                 
+                
     }
     
     exp_dict = experiments[experiment_name]
